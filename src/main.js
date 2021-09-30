ca68434ad47c88cd8b068597ad9ceee415668646
Vlada FKZ Laptop <vlada030@gmail.com>
2021-09-30 14:47:43 +0200

    ucitan font awesome preko scss, postavljen dodatni menu ali MORA da se menja sve

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 import './scss/main.scss'
+
+const button = document.getElementById('socials-btn')
+const wrapper = document.getElementById('container')
+const user = document.getElementById('user')
+const menu = document.getElementById('menu')
+const text = document.getElementById('text')
+
+button.addEventListener('click', () => {
+    // user.style.display = 'none';
+    // menu.style.display = 'flex';
+    wrapper.classList.add('card-footer--active')
+    text.classList.add('card-text--active')
+    menu.style.display = 'flex'
+    user.style.display = 'none'
+
+})
+
